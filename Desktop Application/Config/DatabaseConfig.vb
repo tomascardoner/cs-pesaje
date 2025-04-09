a5f9554db459017190c8893cd76fb939ecfed1af
Tomás Cardoner <tomas@cardoner.com.ar>
2025-04-09 15:08:57 -0300

    Database parameters added

--- Desktop Application/Config/DatabaseConfig.vb
+++ Desktop Application/Config/DatabaseConfig.vb
@@ -1,9 +1,12 @@
 ﻿Public Class DatabaseConfig
     Public Property Datasource As String
     Public Property Database As String
+    Public Property AttachDBFilename As String
+    Public Property IntegratedSecurity As Boolean
     Public Property UserId As String
     Public Property Password As String
     Public Property Provider As String
+    Public Property TrustServerCertificate As Boolean
     Public Property ConnectTimeout As Byte
     Public Property ConnectRetryCount As Byte
     Public Property ConnectRetryInterval As Byte
